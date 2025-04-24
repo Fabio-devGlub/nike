2bae75d89de3d167ac2ebf3cc25bc7f7fa11a457
Fabio-devGlub <Fabiobboy07@gmail.com>
2025-04-24 01:05:48 -0300

    nike Al Star

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".imagem-tenis")
+let botao = document.querySelector(".botao-carrinho")
+
+
+function mudarVisual(cor, imagem){
+    tenis.classList.add("troca-efeito")
+
+    body.style.background = cor
+    
+    botao.style.background = cor
+
+    setTimeout(() => {
+        tenis.src = imagem
+        tenis.classList.remove("troca-efeito")
+    }, 500);
+
+
+   
+    
+            
+    
+    
+}
+
